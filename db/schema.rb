112622cf921fb9f93532ede4a7f45e5b12a062c8
yusuke.honda <14au064f@al.rikkyo.ac.jp>
2019-08-06 05:47:52 +0000

    Kadai-second

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_101913) do
+ActiveRecord::Schema.define(version: 2019_08_06_040816) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
