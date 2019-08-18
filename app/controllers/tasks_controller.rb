11974760b06a219a418652f32b6be4089665dd54
yusuke.honda <14au064f@al.rikkyo.ac.jp>
2019-08-18 11:38:29 +0000

    HIMODUKE

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,22 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build  
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクが正常に投稿されました"
